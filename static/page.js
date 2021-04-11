bd60ccf50b89be6ececa82fa2dcf5c9e775dbcc1
NriotHrreion <nriot233@gmail.com>
2021-04-11 14:36:00 +0800

    upload12

--- static/page.js
+++ static/page.js
@@ -1,1 +1,21 @@
+refreshBodyClassNames();
 
+setTimeout(() => {
+    refreshBodyClassNames();
+
+    document.getElementsByClassName("sidebar-toggle")[0].onclick = function() {
+        if(document.body.className == "ready sticky") {
+            window.localStorage["nocp.sidebarOpen"] = "1";
+        } else if(document.body.className == "ready sticky close") {
+            window.localStorage["nocp.sidebarOpen"] = "0";
+        }
+    };
+}, 100);
+
+function refreshBodyClassNames() {
+    if(window.localStorage["nocp.sidebarOpen"] == "0") {
+        document.body.className = "ready sticky close";
+    } else {
+        document.body.className = "ready sticky";
+    }
+}
